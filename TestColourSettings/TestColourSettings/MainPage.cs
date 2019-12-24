03b93158fbbb6f73d225eae4609a6449fc7341ac
Smartman Apps <20635275+SmartmanApps@users.noreply.github.com>
2019-12-24 16:51:40 +1100

    1st version - direct call to ColourSettingsPage working

--- TestColourSettings/TestColourSettings/MainPage.cs
+++ TestColourSettings/TestColourSettings/MainPage.cs
@@ -3,14 +3,14 @@
 namespace TestColourSettings {public class MainPage :ContentPage {
 
 public MainPage() {
-    Title="TestColourSettings";
+    Title="TestColourSettings MainPage";
     BackgroundColor=Color.Black;
     Label TopLabel=new Label{
         HorizontalTextAlignment=TextAlignment.Center,
         VerticalTextAlignment=TextAlignment.Center
         };
     TopLabel.TextColor=Color.FromHex("009933");
-    TopLabel.Text="TestColourSettings";
+    TopLabel.Text="TestColourSettings MainPage";
 //    var MainStack=new StackLayout{VerticalOptions=LayoutOptions.Center};
 //    MainStack.Children.Add(TopLabel);
 //    Content=MainStack;
